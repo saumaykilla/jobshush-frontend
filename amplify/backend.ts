60eb3108c48d96c777c144531fd19be558f3b6dc
saumay23 <ksaumya1510@gmail.com>
2025-10-30 13:08:43 -0400

    removed queue creation stack from code

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -9,30 +9,12 @@ import { SqsDestination } from 'aws-cdk-lib/aws-s3-notifications';
 
 
 
-const backend = defineBackend({
+defineBackend({
   auth,
   data,
   storage,
 });
 
-
-const uploadQueue = new Queue(
-  backend.storage.stack,
-  'UploadNotificationQueue',
-  {
-    queueName: 'upload-notification-queue',
-    visibilityTimeout: Duration.seconds(300),
-    retentionPeriod: Duration.days(14),
-  }
-);
-
-backend.storage.resources.bucket.addEventNotification(
-  EventType.OBJECT_CREATED,
-  new SqsDestination(uploadQueue),
-  {
-    prefix: 'pdf/',
-  },
-);
 
 
 
